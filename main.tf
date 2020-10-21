a7e2f84517d20901ebdb83a0daab92c9850c5eb0
KalluruS <>
2020-10-21 09:05:14 +0530

    Update terraform-vpc/

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.VPC_CIDR
+  cidr_block      = var.VPC_CIDR
 }
 
 resource "aws_subnet" "private-subnets" {
-  count = length(var.PRIVATE_SUBNETS)
-  cidr_block = element(var.PRIVATE_SUBNETS, count.index)
-  vpc_id = aws_vpc.main.id
+  count           = length(var.PRIVATE_SUBNETS)
+  cidr_block      = element(var.PRIVATE_SUBNETS, count.index)
+  vpc_id          = aws_vpc.main.id
 }
 
 resource "aws_subnet" "public-subnets" {
-  count = length(var.PUBLIC_SUBNETS)
-  cidr_block = element(var.PUBLIC_SUBNETS, count.index)
-  vpc_id = aws_vpc.main.id
+  count           = length(var.PUBLIC_SUBNETS)
+  cidr_block      = element(var.PUBLIC_SUBNETS, count.index)
+  vpc_id          = aws_vpc.main.id
 }
