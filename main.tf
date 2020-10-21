5b371c53da4c9d67d118d4fc687979208a09eb9c
KalluruS <>
2020-10-21 09:09:43 +0530

    Update terraform-vpc/

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
 resource "aws_vpc" "main" {
-  cidr_block      = var.VPC_CIDR
+  cidr_block          = var.VPC_CIDR
 }
 
 resource "aws_subnet" "private-subnets" {
-  count           = length(var.PRIVATE_SUBNETS)
-  cidr_block      = element(var.PRIVATE_SUBNETS, count.index)
-  vpc_id          = aws_vpc.main.id
+  count               = length(var.PRIVATE_SUBNETS)
+  cidr_block          = element(var.PRIVATE_SUBNETS, count.index)
+  vpc_id              = aws_vpc.main.id
+  availability_zone   = element(data.aws_availability_zones.available.id, count.index)
 }
 
 resource "aws_subnet" "public-subnets" {
-  count           = length(var.PUBLIC_SUBNETS)
-  cidr_block      = element(var.PUBLIC_SUBNETS, count.index)
-  vpc_id          = aws_vpc.main.id
+  count               = length(var.PUBLIC_SUBNETS)
+  cidr_block          = element(var.PUBLIC_SUBNETS, count.index)
+  vpc_id              = aws_vpc.main.id
+  availability_zone   = element(data.aws_availability_zones.available.id, count.index)
 }
+
