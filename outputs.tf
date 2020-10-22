4e248b44b3c7e17432a0d5a56fbd9909121f757a
KalluruS <>
2020-10-22 08:11:34 +0530

    Update terraform-vpc/

---
+++ outputs.tf
@@ -0,0 +1,23 @@
+output "VPC_ID" {
+  value = aws_vpc.main.id
+}
+
+output "VPC_CIDR" {
+  value = aws_vpc.main.cidr_block
+}
+
+output "PRIVATE_SUBNETS" {
+  value = aws_subnet.private-subnets.*.id
+}
+
+output "PUBLIC_SUBNETS" {
+  value = aws_subnet.public-subnets.*.id
+}
+
+output "PUBLIC_CIDRS" {
+  value = aws_subnet.public-subnets.*.cidr_block
+}
+
+output "PRIVATE_CIDRS" {
+  value = aws_subnet.private-subnets.*.cidr_block
+}
