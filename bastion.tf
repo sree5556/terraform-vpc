b3bf7ba0a110c8b9c42e2bbe95f15ef5374c5c1e
KalluruS <>
2020-10-27 07:51:34 +0530

    Update terraform/terraform-vpc/

--- bastion.tf
+++ bastion.tf
@@ -4,6 +4,9 @@ resource "aws_instance" "bastion" {
   subnet_id                   = aws_subnet.public-subnets.*.id[0]
   vpc_security_group_ids      = [aws_security_group.allow-ssh-bastion.id]
   associate_public_ip_address = true
+  tags                        = {
+          Name                = "bastion"
+  }
 }
 
 resource "aws_security_group" "allow-ssh-bastion" {
