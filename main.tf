18c224323c37010d4271a0ba476716c2a0e65a07
SriNivas <sreenivasyandamuri44@gmail.com>
2020-10-29 12:44:05 +0530

    vpc

--- main.tf
+++ main.tf
@@ -1,30 +1,30 @@
 resource "aws_vpc" "main" {
-  cidr_block          = var.VPC_CIDR
+  cidr_block          = "192.168.0.0/24"
   tags                = {
-    Name              = "roboshop-vpc-${var.ENV}"
-    Environment       = var.ENV
+    Name              = "roboshop-vpc" //-${var.ENV}"
+    Environment       =  DEV //var.ENV
   }
 }
 
-resource "aws_subnet" "private-subnets" {
-  count               = length(var.PRIVATE_SUBNETS)
-  cidr_block          = element(var.PRIVATE_SUBNETS, count.index)
-  vpc_id              = aws_vpc.main.id
-  availability_zone   = element(data.aws_availability_zones.available.names, count.index)
-  tags                = {
-    Name              = "private-subnet-${count.index+1}"
-    Environment       = var.ENV
-  }
-}
-
-resource "aws_subnet" "public-subnets" {
-  count               = length(var.PUBLIC_SUBNETS)
-  cidr_block          = element(var.PUBLIC_SUBNETS, count.index)
-  vpc_id              = aws_vpc.main.id
-  availability_zone   = element(data.aws_availability_zones.available.names, count.index)
-  tags                = {
-    Name              = "public-subnet-${count.index+1}"
-    Environment       = var.ENV
-  }
-}
+//resource "aws_subnet" "private-subnets" {
+//  count               = length(var.PRIVATE_SUBNETS)
+//  cidr_block          = element(var.PRIVATE_SUBNETS, count.index)
+//  vpc_id              = aws_vpc.main.id
+//  availability_zone   = element(data.aws_availability_zones.available.names, count.index)
+//  tags                = {
+//    Name              = "private-subnet-${count.index+1}"
+//    Environment       = var.ENV
+//  }
+//}
+//
+//resource "aws_subnet" "public-subnets" {
+//  count               = length(var.PUBLIC_SUBNETS)
+//  cidr_block          = element(var.PUBLIC_SUBNETS, count.index)
+//  vpc_id              = aws_vpc.main.id
+//  availability_zone   = element(data.aws_availability_zones.available.names, count.index)
+//  tags                = {
+//    Name              = "public-subnet-${count.index+1}"
+//    Environment       = var.ENV
+//  }
+//}
 
