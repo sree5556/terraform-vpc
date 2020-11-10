3680c6e946028424f8e4c8a0f680421c205f5097
SriNivas <sreenivasyandamuri44@gmail.com>
2020-11-10 20:48:21 +0530

    count

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main" {
   cidr_block          = var.VPC_CIDR
   tags                = {
     Name              = "roboshop-vpc-${var.ENV}"
-    Environment       =  var.ENV
+    Environment       = var.ENV
   }
 }
 
